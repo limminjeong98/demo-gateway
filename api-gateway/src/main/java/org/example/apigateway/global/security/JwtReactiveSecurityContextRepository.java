739f1d91ecce73966dbafa1444d3c439a18e633d
limminjeong98 <limminjeong98@naver.com>
2024-12-16 10:41:14 +0900

    feat: spring security를 활용한 인증 및 인가 구현

---
+++ api-gateway/src/main/java/org/example/apigateway/global/security/JwtReactiveSecurityContextRepository.java
@@ -0,0 +1,52 @@
+package org.example.apigateway.global.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class JwtReactiveSecurityContextRepository implements ServerSecurityContextRepository {
+
+    private final JwtUtil jwtUtil;
+    private final ReactiveUserDetailsService userDetailsService;
+
+    public JwtReactiveSecurityContextRepository(JwtUtil jwtUtil, ReactiveUserDetailsService userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    public Mono<Void> save(ServerWebExchange exchange, SecurityContext context) {
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<SecurityContext> load(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        // Bearer abcdef
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            // 인증 로직 체크
+            String authToken = authHeader.substring(7);
+            try {
+                String username = jwtUtil.extractUsername(authToken);
+
+                if (username != null && jwtUtil.validateToken(authToken)) {
+                    return userDetailsService.findByUsername(username)
+                            .map(userDetails -> new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()))
+                            .map(SecurityContextImpl::new);
+                }
+            } catch (Exception e) {
+                log.error("Authentication Error: ", e);
+            }
+        }
+
+        return Mono.empty();
+    }
+}
